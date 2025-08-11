7fb54810bed31f1fb0795bdffc3458bb9095cb3c
RonnyIbarra <rjibarra1@espe.edu.ec>
2025-08-10 19:14:43 -0500

    Error corregido

--- math.js
+++ math.js
@@ -1,15 +1,14 @@
 function factorial(n) {
     if (n < 0) throw new Error('El número debe ser no negativo');
     if (n === 0 || n === 1) return 1;
-    return n + factorial(n - 1);  // Error Intecional
+    return n * factorial(n - 1);  // Corregido
 }
 
 function fibonacci(n) {
     if (n < 0) throw new Error('El número debe ser no negativo');
     if (n === 0) return 0;
     if (n === 1) return 1;
-    return n * factorial(n - 1);//Error corregido
-
+    return fibonacci(n - 1) + fibonacci(n - 2);  // Corregido
 }
 
 module.exports = { factorial, fibonacci };
